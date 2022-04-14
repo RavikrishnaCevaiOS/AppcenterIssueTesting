9470c55298e735e859ac0dd63aa1259b8581ed01
RavikrishnaCevaiOS <ravikrishnak@cevasoftware.com>
2022-04-14 17:42:41 +0530

    Initial Commit

---
+++ SamplePro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SamplePro
+//
+//  Created by Ravi Krishna on 14/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
