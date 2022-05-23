e1dfa42f46b142a2f551538f12fbedc0421e4dc4
RavikrishnaCevaiOS <ravikrishnak@cevasoftware.com>
2022-05-23 14:39:18 +0530

    removed some VCs and added few more VCs

--- SamplePro/DashboardVC.swift
+++ SamplePro/DashboardVC.swift
@@ -1,21 +1,22 @@
 //
-//  ViewController.swift
+//  DashboardVC.swift
 //  SamplePro
 //
-//  Created by Ravi Krishna on 14/04/22.
+//  Created by Ravi Krishna on 23/05/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DashboardVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
-        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "SamVC") as? SamVC
+        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "YellowVC") as? YellowVC
         self.navigationController?.pushViewController(vc!, animated: true)
     }
-}
 
+}
---
+++ SamplePro/YellowVC.swift
@@ -0,0 +1,18 @@
+//
+//  YellowVC.swift
+//  SamplePro
+//
+//  Created by Ravi Krishna on 23/05/22.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
